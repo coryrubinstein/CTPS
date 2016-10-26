6aeb82458d0da791323df0337e3d22df9979179e
coryrubinstein <cory_rubinstein@cbmg.com>
2016-10-26 16:30:44 -0700

    Added the solutions and fixed some broken relationships

--- web/js/test.js
+++ web/js/test.js
@@ -7,7 +7,13 @@ $(document).ready(function () {
         type: 'GET',
         cache: false,
         success: function(data) {
-           $(data.Issue.reasons).each(function (index, value) {
+           $(data.Issue.reasons).each(function (index, value)
+           {
+               $('#leadership').click(function()
+               {
+                  $('#leadReas').slideDown(1550);
+
+               });
 
                if (value.familyId == 104)
                {
@@ -16,42 +22,74 @@ $(document).ready(function () {
                     Leadership Reasons.........................
                     */
 
-                   $('#Leadership').text(value.family);
+                   $('#leadership').text(value.family);
                    var leadReasonId = 'reas_lead_'+value.id;
-                   $('#LeadReas').append('<li>'+value.name+'<ul id="'+ leadReasonId +'"></ul></li><br><br>');
+                   $('#leadReas').append('<li>'+value.name+'<ul id="'+ leadReasonId +'"></ul></li><br><br>');
 
                    /*
                    Leadership Subreasons...................
                     */
 
-                      var subreasons = value.subreasons;
-                      for (i = 0; i < subreasons.length; i++ )
+                      var leadSubReasons = value.subreasons;
+                      for (i = 0; i < leadSubReasons.length; i++ )
                       {
-                          $('#'+leadReasonId).append('<br><li type="disc">'+subreasons[i].name+'</li>');
+                          $('#'+leadReasonId).append('<br><li type="disc">'+leadSubReasons[i].name+'</li>');
+
+                          /*
+                           Leadership Solutions...................
+                           */
+
+                          var leadSolutions = leadSubReasons[i].solutions;
+
+                          for (x = 0; x < leadSolutions.length; x++ )
+                          {
+                              $('#'+leadReasonId).append('<br><ul><li type="circle">'+leadSolutions[x].name+'</li></ul>');
+                          }
+
 
                       }
 
                }
 
+
+
                if (value.familyId == 105)
                {
+
+                   $('#front_Line').click(function()
+                   {
+                       $('#frontReas').slideDown(1550);
+                   });
 
                    /*
                     Frontline Reasons.........................
                     */
 
-                   $('#Front_Line').text(value.family);
+                   $('#front_Line').text(value.family);
                    var frontReasonId = 'reas_front_'+value.id;
-                   $('#FrontReas').append('<li>'+value.name+'<ul type="none" id="'+ frontReasonId + '"></ul></li><br><br>');
+                   $('#frontReas').append('<li>'+value.name+'<ul type="none" id="'+ frontReasonId + '"></ul></li><br><br>');
 
                    /*
                     Frontline Subreasons...................
                     */
 
-                   var subreasons = value.subreasons;
-                   for (i = 0; i < subreasons.length; i++ )
+                   var frontSubreasons = value.subreasons;
+                   for (i = 0; i < frontSubreasons.length; i++ )
                    {
-                       $('#'+frontReasonId).append('<br><li type="disc">'+subreasons[i].name+'</li>');
+                       $('#'+frontReasonId).append('<br><li type="disc">'+frontSubreasons[i].name+'</li>');
+
+
+                       /*
+                        Frontline Solutions...................
+                        */
+
+                       var frontSolutions = frontSubreasons[i].solutions;
+
+                       for (x = 0; x < frontSolutions.length; x++ )
+                       {
+                           $('#'+frontReasonId).append('<br><ul><li type="circle">'+frontSolutions[x].name+'</li></ul>');
+                       }
+
 
                    }
 
@@ -60,30 +98,51 @@ $(document).ready(function () {
                if (value.familyId == 106)
                {
 
+                   $('#behavioral').click(function()
+                   {
+                       $('#behaveReas').slideDown(1550);
+                   });
+
                    /*
                     Behavioral Reasons.........................
                     */
 
-                   $('#Behavioral').text(value.family);
+                   $('#behavioral').text(value.family);
                    var behaveReasonId = 'reas_behave_'+value.id;
-                   $('#BehaveReas').append('<li>'+value.name+'<ul type="none" id="'+ behaveReasonId + '"></ul></li><br><br>');
+                   $('#behaveReas').append('<li>'+value.name+'<ul type="none" id="'+ behaveReasonId + '"></ul></li><br><br>');
 
                    /*
                     Behavioral Subreasons.........................
                     */
 
-                   var subreasons = value.subreasons;
-                   for (i = 0; i < subreasons.length; i++ )
+                   var behaveSubreasons = value.subreasons;
+                   for (i = 0; i < behaveSubreasons.length; i++ )
                    {
-                       $('#'+behaveReasonId).append('<br><li type="disc">'+subreasons[i].name+'</li>');
+                       $('#'+behaveReasonId).append('<br><li type="disc">'+behaveSubreasons[i].name+'</li>');
+
+
+                       /*
+                        Behavioral Solutions...................
+                        */
+
+                       var behaveSolutions = behaveSubreasons[i].solutions;
+
+                       for (x = 0; x < behaveSolutions.length; x++ )
+                       {
+                           $('#'+behaveReasonId).append('<br><ul><li type="circle">'+behaveSolutions[x].name+'</li></ul>');
+                           // console.log(behaveSolutions[x].name);
+                       }
+
 
                    }
 
                }
+
+
+
+
             });
-            //console.log(data.Issue);
-           // console.log(value.name);
-            //value.familyId == 104
+
         }
 
     })
